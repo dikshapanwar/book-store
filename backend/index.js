3cad84a75f262f22a8778594cea77012a87458a2
diksha <diksha.panwarnhn@outlook.com>
2024-11-12 11:04:22 +0530

    Connect MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
 import express from 'express'
+import mongoose, { connect } from 'mongoose'
+import connectDB from './db/db.js'
+import dotenv from 'dotenv'
+dotenv.config()
 const app = express()
 const port =process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
+connectDB()
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
