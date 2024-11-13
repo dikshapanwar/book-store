550bcaaca454c60492e9afaa644225a6124f1b58
diksha <diksha.panwarnhn@outlook.com>
2024-11-13 09:20:00 +0530

    change

--- frontend/src/firebase/firebase.js
+++ frontend/src/firebase/firebase.js
@@ -5,14 +5,6 @@ import {getAuth} from "firebase/auth"
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDH22Ns5LNSNGDPTd0u3Wdce9oOvmK9IwI",
-  authDomain: "book-store-9925b.firebaseapp.com",
-  projectId: "book-store-9925b",
-  storageBucket: "book-store-9925b.firebasestorage.app",
-  messagingSenderId: "882972585672",
-  appId: "1:882972585672:web:44c09819456e61b5ff7ffd"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
